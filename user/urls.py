127ac29841fc13a4b75b0d5a30d3e93d9e81fe79
chandan91077 <153637267+chandan91077@users.noreply.github.com>
2026-02-22 01:33:01 +0530

    Add SMTP password reset flow and registration template styling

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -9,6 +10,38 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact_us, name='contact_us'),
     path('login/', views.login_view, name='login'),
+    path(
+        'forgot-password/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html',
+            email_template_name='registration/password_reset_email.html',
+            subject_template_name='registration/password_reset_subject.txt',
+            success_url=reverse_lazy('password_reset_done'),
+        ),
+        name='password_reset',
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html',
+            success_url=reverse_lazy('password_reset_complete'),
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete',
+    ),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('set-delivery-pincode/', views.set_delivery_pincode, name='set_delivery_pincode'),
